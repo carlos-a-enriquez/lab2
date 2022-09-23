86a118aa87823a9f0aad9e2bddcab1ce8dedb5d8
Carlos <carlos.enriquez9896@gmail.com>
2022-09-23 19:16:16 +0200

    Setting up base SVM training functions

--- scripts/svm_train.py
+++ scripts/svm_train.py
@@ -24,10 +24,36 @@ def extract_true_classes(training_fh):
 	return np.array(list(train['Class']))
 	
 	
-def grid_search_validate():
+def cross_validation_init_grid(x,y)
+	
+	
+def grid_search_validate(x, y, k_list, c_list, gamma_list):
 	"""
+	This function is meant to implement the grid search for the
+	tuning of different SVM hyperparameters. The input should be the following:
+	
+	k_list = An array type object that contains the list of k values to be evaluated
+	
+	c_list =  An array type object that contains the list of C (soft margin) values 
+	to be evaluated
+	
+	gamma_list = An array type object that contains the list of gamma values to 
+	be evaluated (for the RBF kernel)
+	
+	X= Numpy array containing the 20-dimensional vectors corresponding to each
+	training example. 
+	
+	Y = Numpy array containing the true classes corresponding to each training 
+	example.
 	"""
-	return None
+	#Creating the combination of hyperparameters
+	hyper_param = [(k,c,g) for k in k_list for c in c_list for g in gamma_list]
+	
+	#iterating over all hyperparameter combinations
+	for comb in hyper_param:
+		
+		
+	
 
 
 
