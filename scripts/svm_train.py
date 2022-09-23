5961723eeb2877d38ffbbbe5a4527355825e0e6c
Carlos <carlos.enriquez9896@gmail.com>
2022-09-23 23:05:55 +0200

    Added the first printing of results

--- scripts/svm_train.py
+++ scripts/svm_train.py
@@ -4,10 +4,13 @@ import sys, os
 import pandas as pd
 import numpy as np
 from sklearn import svm
+from sklearn.metrics import confusion_matrix
 #import matplotlib.pyplot as plt
 #import seaborn as sn
 import environmental_variables as env
 import svm_encode as enco
+from cross_validation import graphics_confusion_matrix
+from accuracy import get_mcc
 
 
 def extract_true_classes(training_fh):
@@ -67,21 +70,38 @@ def cross_validation_init_grid(sequences,Y, folds, unique_folds, hyper_param_dic
 	#Obtaining X, the 2D array of 20-dim vectors per example
 	X = enco.encode_sequences(sequences, hyper_param_dict['K'], env.alphabet)
 	
+	#List of MCC values
+	MCC_list = list()
+	
 	#Cross validation iteration
 	for f in unique_folds:
 		#Separating training and testing
 		test_indeces = np.where(folds == f)[0] #extract np array indeces where folds == current f
 		train_indeces = np.where(folds != f)[0]
 		test_iter_X = X[test_indeces, :]
-		test_iter_Y = Y[test_indeces]
+		test_iter_Y = Y[test_indeces] #should be true classes
 		train_iter_X = X[train_indeces, :]
-		train_iter_Y = Y[train_indeces]
+		train_iter_Y = Y[train_indeces] #should be true classes
 		
 		#Define the model
 		mySVC = svm.SVC(C=hyper_param_dict['C'], kernel='rbf', gamma=hyper_param_dict['Gamma'])
 		
 		#Train the model
 		mySVC.fit(train_iter_X, train_iter_Y)
+		
+		#Predict on test set
+		y_pred_test = mySVC.predict(test_iter_X)
+		
+		#Validate
+		cm = confusion_matrix(test_iter_Y, y_pred_test)
+		graphics_confusion_matrix(cm, "N/A", image_folder_path, "svm_fold_%s"%(str(f)))
+		MCC_list.append(get_mcc(cm)) #Mathew's correlation
+	
+	#List to numpy
+	MCC_list = np.array(MCC_list[:])
+	
+	return np.mean(MCC_list)
+		
 		
 	
 	
@@ -115,10 +135,15 @@ def grid_search_validate(sequences, Y, k_list, c_list, gamma_list, folds, unique
 	#Creating the combination of hyperparameters
 	hyper_param = [(k,c,g) for k in k_list for c in c_list for g in gamma_list]
 	
+	#Hyperparameter MCC list
+	hyper_results = list()
+	
 	#iterating over all hyperparameter combinations
 	for comb in hyper_param:
 		hyper_param_dict = dict(K=comb[0], C = comb[1], Gamma = comb[2])
-		cross_validation_init_grid(sequences,Y, folds, unique_folds, hyper_param_dict)
+		hyper_results.append(cross_validation_init_grid(sequences,Y, folds, unique_folds, hyper_param_dict))
+		
+	return hyper_param, hyper_results, max(hyper_results)
 		
 		
 		
@@ -131,15 +156,21 @@ def grid_search_validate(sequences, Y, k_list, c_list, gamma_list, folds, unique
 if __name__ == "__main__":
 	#Opening the input examples file and defining the output image folder path
 	try:
-		train_fh = sys.argv[1]		
+		train_fh = sys.argv[1]
+		image_folder_path = sys.argv[2]		
 	except IndexError:
 		train_fh = input("insert the training data path   ")
+		image_folder_path = input("insert the output image folder path  ")
+		
+	if image_folder_path[-1] != "/":
+		image_folder_path += "/"		
 		
 	#Encoding workflow
 	sequences = enco.extract_sequences(train_fh)
 	train_Y = extract_true_classes(train_fh)	
 	folds, unique_folds = extract_fold_info(train_fh)
-	grid_search_validate(sequences, train_Y, env.k_list, env.c_list, env.gamma_list, folds, unique_folds)
+	comb, mccs, best_mcc= grid_search_validate(sequences, train_Y, env.k_list, env.c_list, env.gamma_list, folds, unique_folds)
+	print("\nModel Tuning results:\nCombinations: %s\nMCC scores: %s\nBest score: %0.2f"%(str(comb), str(mccs), best_mcc))
 	
 	
 	
