fd6f5361194dfc7bc7120db50a80cbcc852b4fbd
carlos-a-enriquez <carlos.enriquez9896@gmail.com>
2022-10-01 18:42:46 +0200

    adding real negative and real positive output to misclassified

--- scripts/misclassified.py
+++ scripts/misclassified.py
@@ -4,6 +4,8 @@ This module is meant to create a text file with the UNIPROT
 accession number of those sequences that were misclassified.
 
 Therefore, a false positive and false negative file will be created. 
+In addition, the set of real negatives (TN+FP) and real positives (TP+FN)
+will also be returned. 
 """
 
 import sys
@@ -20,42 +22,65 @@ def extract_missclass(bench_fh):
 	bench = bench = pd.read_csv(bench_fh)
 	false_positives = bench.query('scores == 1 and `Class` == \"NO_SP\"').loc[:, 'UniProtKB accession'].tolist()
 	false_negatives = bench.query('scores == 0 and `Class` == \"SP\"').loc[:, 'UniProtKB accession'].tolist()
+	real_negatives = bench.query('`Class` == \"NO_SP\"').loc[:, 'UniProtKB accession'].tolist()
+	real_positives = bench.query('`Class` == \"SP\"').loc[:, 'UniProtKB accession'].tolist()
 	
-	return false_positives, false_negatives
+	lists = false_positives, false_negatives, real_negatives, real_positives
+	
+	return lists
     
     
-def print_files(false_positives, false_negatives, export_pos, export_neg):
+def print_files(lists, export_path_s):
 	"""
 	Prints the false positive and false negative files
+	
+	lists = Tuple containing the following:
+	false_positives, false_negatives, real_negatives, real_positives
+	
+	export_path = Path were all the text files will be saved.
 	"""
+	false_positives, false_negatives, real_negatives, real_positives = lists
 	#False positives
-	with open(export_pos, 'w') as f:
+	with open(export_path_s+'false_positives.txt', 'w') as f:
 		for accession in false_positives:
 			f.write(accession.strip()+'\n')
 			
 	#False negatives
-	with open(export_neg, 'w') as f:
+	with open(export_path_s+'false_negatives.txt', 'w') as f:
 		for accession in false_negatives:
 			f.write(accession.strip()+'\n')
+	
+	#Real negatives
+	with open(export_path_s+'real_negatives.txt', 'w') as f:
+		for accession in real_negatives:
+			f.write(accession.strip()+'\n')
+			
+	#Real positives
+	with open(export_path_s+'real_positives.txt', 'w') as f:
+		for accession in real_positives:
+			f.write(accession.strip()+'\n')
+	
 			
 	
 
 if __name__ == '__main__':
 	try:
 		bench_pred_fh = sys.argv[1] #CSV file that contains both the original data and the SVM predictions
-		export_positive = sys.argv[2] #path where the new false positive will be added
-		export_negative = sys.argv[3] #path where the new false negatives will be added
+		export_path = sys.argv[2] #path for: false_positives, false_negatives, real_negatives, real_positives
 		
 		
 	except IndexError:
 		bench_pred_fh = input("Insert the benchmark csv file with predictions   ")
-		export_positive = input("insert the export path where the false positive entries will be added     ")
-		export_negative = input("insert the export path where the false negatives entries will be added    ")
+		export_path = input("insert the export path where the files will be exported     ")
+		
 	
+	
+	if export_path[-1] != "/":
+		export_path += "/"		
 	
 	#Run flow
-	fp, fn = extract_missclass(bench_pred_fh)
-	print_files(fp, fn, export_positive, export_negative)
+	accessions = extract_missclass(bench_pred_fh)
+	print_files(accessions, export_path)
 	
 	
 	
